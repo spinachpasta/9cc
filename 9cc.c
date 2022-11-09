653121413546a97c4066c72825c38a3ff4e8cc94
spinachpasta <49642352+spinachpasta@users.noreply.github.com>
2022-11-10 00:26:13 +0900

    addition and subtraction

--- 9cc.c
+++ 9cc.c
@@ -1,16 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
+int isDigit(char c);
+
+int parseInt(char *str)
+{
+  int result = 0;
+  while (1)
+  {
+    if (!isDigit(*str))
+    {
+      break;
+    }
+    else
+    {
+      int digit = *str - '0';
+      result = result * 10 + digit;
+    }
+    str++;
+  }
+  return result;
+}
+int intLength(char *str)
+{
+  int length = 0;
+  while (1)
+  {
+    if (!isDigit(*str))
+    {
+      break;
+    }
+    length++;
+    str++;
+  }
+  return length;
+}
+int isDigit(char c)
+{
+  return '0' <= c && c <= '9';
+}
+
+int main(int argc, char **argv)
+{
+  if (argc != 2)
+  {
     fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
   }
 
+  char *p = argv[1];
+
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  {
+    int parsednum = parseInt(p);
+    int parsedlength = intLength(p);
+    p += parsedlength;
+    printf("  mov rax, %ld\n", parsednum);
+  }
+  while (*p)
+  {
+    if (*p == '+')
+    {
+      p++;
+      {
+        int parsednum = parseInt(p);
+        int parsedlength = intLength(p);
+        p += parsedlength;
+        printf("  add rax, %ld\n", parsednum);
+      }
+      continue;
+    }
+
+    if (*p == '-')
+    {
+      p++;
+      {
+        int parsednum = parseInt(p);
+        int parsedlength = intLength(p);
+        p += parsedlength;
+        printf("  sub rax, %ld\n", parsednum);
+      }
+      continue;
+    }
+
+    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+    return 1;
+  }
+
   printf("  ret\n");
   return 0;
 }
--- test.py
+++ test.py
@@ -1,5 +1,15 @@
 
 import os
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def check(input: str, expected: int):
     os.system(f"./9cc {input} > tmp.s")
@@ -7,14 +17,19 @@ def check(input: str, expected: int):
     returned_value = (os.system("./tmp") >> 8) & 0xff
 
     if expected == returned_value:
+        print(f"{bcolors.OKGREEN}passed:{input=} {expected=} {bcolors.ENDC}")
         return True
 
-    print(f"check:{input=} {expected=} {returned_value=}")
+    print(f"{bcolors.FAIL}FAIL:check:{input=} {expected=} {returned_value=}{bcolors.ENDC}")
     return False
 
 
 assert check("0", 0)
 
 assert check("42", 42)
+
+assert check("0+10+3", 0+10+3)
+
+assert check("111+10-42", 111+10-42)
 
 print("OK")
